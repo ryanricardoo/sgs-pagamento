1b00cb9c69499f2c2c37ad0e0891ca2bb40485ba
unknown <ricardoryan2403@gmail.com>
2026-05-01 16:06:11 -0300

    feat: inicializa arquitetura base e pacotes do sistema

---
+++ src/test/java/com/sgs/SgsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
