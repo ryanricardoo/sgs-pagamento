e58f34ffafd1c914111efebd0ea2978335cf2c7c
unknown <ricardoryan2403@gmail.com>
2026-05-03 22:35:35 -0300

    feat: implementa repository com native query

---
+++ src/main/java/com/sgs/repository/SolicitacaoRepository.java
@@ -0,0 +1,19 @@
+package com.sgs.repository;
+
+import com.sgs.model.Solicitacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface SolicitacaoRepository extends JpaRepository<Solicitacao, Long> {
+
+    @Query(value = "SELECT S.* FROM tb_solicitacao s " +
+            "INNER JOIN tb_solicitante sol ON s.solicitante_id = sol.id " +
+            "WHERE sol.cpf_cnpj = :documento",
+            nativeQuery = true)
+    List<Solicitacao> buscarCpfCnpj(@Param("documento") String documento);
+}
